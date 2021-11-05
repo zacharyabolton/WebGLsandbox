5c99bdb075dd4194852360e6a2d2b8d1948ff372
Zac Bolton <zacbolton2129@gmail.com>
2021-11-04 21:12:14 -0300

    Setting up skeleton project

--- test/tests.js
+++ test/tests.js
@@ -1,16 +1,38 @@
-const chai = require('chai');
 const chaiHttp = require('chai-http');
+const chaiDom = require('chai-dom');
 const assert = require('assert');
+const { expect } = require('chai');
+const chai = require('chai');
+const { JSDOM } = require('jsdom');
+require('jsdom-global')();
 
 chai.use(chaiHttp);
+chai.use(chaiDom);
 
 describe('Test top level / route', () => {
     it('it should have a 200 status code', (done) => {
-        chai.request('http://localhost:8086') // the top level web address
-            .get('/') // the route to add to the top level address
-            .end((err, res) => { // what to do once the request returns
-                assert.equal(res.status, 200); // check we have the 200 OK HTTP code
-                done(); // finish up
+        // the top level web address
+        chai.request('http://localhost:8086')
+            // the route to add to the top level address
+            .get('/')
+            // what to do once the request returns
+            .end((err, res) => {
+                // check we have the 200 OK HTTP code
+                assert.equal(res.status, 200);
+                // finish up
+                done();
             });
     });
+    it('it should have content', (done) => {
+        // the top level web address
+        chai.request('http://localhost:8086')
+            // the route to add to the top level address
+            .get('/')
+            .end((err, res) => {
+                const dom = new JSDOM(res.text);
+                const element = dom.window.document.querySelector('h1');
+                expect(element).to.have.text("WebGL sandbox")
+                //assert.equal(res.body, expected)
+            })
+    })
 })
